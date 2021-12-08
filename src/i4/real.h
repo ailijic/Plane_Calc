c624a096514a5dcff559481970539f3d336bca24
ANDREW ILIJIC <3759927+ailijic@users.noreply.github.com>
2021-12-08 13:48:34 +0000

    Updated real.h to be mathematically pure

--- src/i4/real.h
+++ src/i4/real.h
@@ -1,7 +1,5 @@
 #pragma once
 
-// TODO: Make pure again
-
 #include "i4/i4.h"
 
 #ifdef __cplusplus
@@ -14,21 +12,21 @@ typedef struct Real {
   real t;
 } Real;
 
-Real* REAL(ctor)(Real const* a_this);
-void  REAL(dtor)(Real* a_this);
-Real* REAL(init)(Real* a_this, real a_value);
+Real REAL(ctor)(Real* a_this);
+void REAL(dtor)(Real* a_this);
+Real REAL(init)(Real* a_this, real a_value);
 
-real REAL(convert)(Real*a_this);
+real REAL(convert)(Real a_this) CONST;
 
-Real* REAL(absValue)(Real* a_out, Real const* a_value);
-Real*      REAL(neg)(Real* a_out, Real const* a_value);
-Real*        REAL(e)(Real* a_out, Real const* a_value);  // e^a_value
+Real REAL(absValue)(Real a_value) CONST;
+Real      REAL(neg)(Real a_value) CONST;
+Real        REAL(e)(Real a_value) CONST;  // e^a_value
 
-bool  REAL(isEqual)(Real const* a_lhs, Real const* a_rhs);
-Real*     REAL(max)(Real* a_out, Real const* a_lhs, Real const* a_rhs);
-Real*     REAL(min)(Real* a_out, Real const* a_lhs, Real const* a_rhs);
+bool  REAL(isEqual)(Real a_lhs, Real a_rhs)  CONST;
+Real      REAL(max)(Real a_lhs, Real a_rhs)  CONST;
+Real      REAL(min)(Real a_lhs, Real a_rhs)  CONST;
 
-Real* REAL(add)(Real* a_out, Real const* a_lhs, Real const* a_rhs);
-Real* REAL(sub)(Real* a_out, Real const* a_lhs, Real const* a_rhs);
-Real* REAL(mul)(Real* a_out, Real const* a_lhs, Real const* a_rhs);
-Real* REAL(div)(Real* a_out, Real const* a_lhs, Real const* a_rhs);
+Real REAL(add)(Real a_lhs, Real a_rhs) CONST;
+Real REAL(sub)(Real a_lhs, Real a_rhs) CONST;
+Real REAL(mul)(Real a_lhs, Real a_rhs) CONST;
+Real REAL(div)(Real a_lhs, Real a_rhs) CONST;
